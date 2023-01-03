1c07a71484310cc2154409cfbe5a589c3ba9f7aa
Rahul Bhiwre <51237167+rahulbhiwre@users.noreply.github.com>
2023-01-03 20:05:48 +0530

    added http post request method

--- src/main/java/com/cap/main/Github.java
+++ src/main/java/com/cap/main/Github.java
@@ -4,32 +4,59 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
 import org.json.JSONException;
-import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 public class Github {
 
-	public static void main(String[] args) throws IOException, URISyntaxException, JSONException {
+	public static void main(String[] args) throws IOException, URISyntaxException, JSONException, InterruptedException {
 		System.out.println("Hello Actions");
-		postGit();
+//		getRequestGitHubApi();
+		postRequestGitHubApi();
 	}
 
-	public static void postGit() throws IOException, URISyntaxException, JSONException {
+	private static void postRequestGitHubApi() throws MalformedURLException, IOException, InterruptedException {
+
+		HttpClient client = HttpClient.newHttpClient();
+
+		String url = "https://api.github.com/repos/rahulbhiwre/QuizRestApi/branches/Dev01/rename";
+
+		HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url))
+				.POST(HttpRequest.BodyPublishers.ofString("{\"new_name\":\"hello\"}"))
+				.header("Authorization",
+						"Basic " + Base64.getEncoder()
+								.encodeToString(("rahulbhiwre:TOKEN_HERE").getBytes()))
+				.build();
+
+		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+		System.out.println("StatusCode : " + response.statusCode());
+		System.out.println(response.body());
+
+	}
+
+	public static void getRequestGitHubApi() throws IOException, URISyntaxException, JSONException {
 		HttpURLConnection httpClient = null;
 		String githubUrl = "https://api.github.com/repos/rahulbhiwre/QuizRestApi/branches";
 		httpClient = (HttpURLConnection) new URL(githubUrl).openConnection();
 
 		String userName = "rahulbhiwre";
-		String password = "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";
+		String password = "TOKEN_HERE";
 		String auth = userName + ":" + password;
 		byte[] encodedAuth = org.apache.commons.codec.binary.Base64
 				.encodeBase64(auth.getBytes(StandardCharsets.ISO_8859_1));
 		String authHeader = "Basic " + new String(encodedAuth);
-
+		httpClient.setRequestMethod("GET");
 		httpClient.setRequestProperty("Authorization", authHeader);
 		httpClient.setDoOutput(true);
 		int responseCode = httpClient.getResponseCode();
@@ -45,6 +72,7 @@ public class Github {
 
 			JSONParser parser1 = new JSONParser();
 			Object json = parser1.parse(response.toString());
+
 			System.out.println(json);
 
 		} catch (Exception e) {
